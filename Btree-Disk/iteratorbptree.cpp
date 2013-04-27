45951b4c6dfe299d3b7a5546f2e724d6e1a7ffde
Jan Quequezana <jan.quequezana@ucsp.edu.pe>
2013-04-26 20:27:01 -0500

    Iterator

---
+++ Btree-Disk/iteratorbptree.cpp
@@ -0,0 +1,9 @@
+//
+//  iteratorbptree.cpp
+//  Btree-Disk
+//
+//  Created by Jan Quequezana on 26/04/13.
+//  Copyright (c) 2013 Jan Quequezana. All rights reserved.
+//
+
+#include "iteratorbptree.h"
---
+++ Btree-Disk/iteratorbptree.h
@@ -0,0 +1,14 @@
+//
+//  iteratorbptree.h
+//  Btree-Disk
+//
+//  Created by Jan Quequezana on 26/04/13.
+//  Copyright (c) 2013 Jan Quequezana. All rights reserved.
+//
+
+#ifndef __Btree_Disk__iteratorbptree__
+#define __Btree_Disk__iteratorbptree__
+
+#include <iostream>
+
+#endif /* defined(__Btree_Disk__iteratorbptree__) */
